2dd53831f325646d50287f087096dce1c23b9b8d
HayleyBillingham <hbillingham80@gmail.com>
2023-07-17 13:50:37 +0100

    menu items

--- udemy/game-development-in-js-ts-the-complete-guide/0-flappyBirdClone/src/scenes/BaseScene.js
+++ udemy/game-development-in-js-ts-the-complete-guide/0-flappyBirdClone/src/scenes/BaseScene.js
@@ -4,10 +4,20 @@ class BaseScene extends Phaser.Scene{
     constructor(key,config){
         super(key);
         this.config = config;
+        this.screenCentre = [config.width/2, config.height/2]
     }
 
     create(){
         this.add.image(0, 0, "sky").setOrigin(0, 0); 
+    }
+
+    createMenu(menu){
+        let lastMenuPositionY = 0;
+        menu.forEach(menuItem => {
+            const menuPosition = [this.screenCentre[0], this.screenCentre[1] + lastMenuPositionY]
+            this.add.text(...menuPosition, menuItem.text, {fontSize: "32px", fill:"#CD00FF"}).setOrigin(0.5,1);
+            lastMenuPositionY +=42;
+        });
     }
 }
 
