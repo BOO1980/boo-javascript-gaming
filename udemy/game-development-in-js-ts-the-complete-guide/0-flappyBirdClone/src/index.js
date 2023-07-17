bf3ceed982565bf6e2fe628240f1e3bd8327c38b
HayleyBillingham <hbillingham80@gmail.com>
2023-07-17 12:40:58 +0100

    changed file names

--- udemy/game-development-in-js-ts-the-complete-guide/0-flappyBirdClone/src/index.js
+++ udemy/game-development-in-js-ts-the-complete-guide/0-flappyBirdClone/src/index.js
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
-import PlayScene from "./scenes/playScene";
-import MenuScene from "./scenes/menuScene";
+import PlayScene from "./scenes/PlayScene";
+import MenuScene from "./scenes/MenuScene";
+import PreloadScene from "./scenes/PreloadScene";
 
 const WIDTH = 800;
 const HEIGHT = 600;
@@ -22,7 +23,7 @@ const config = {
 			debug: true,
 		},
 	},
-	scene: [new MenuScene(SHARED_CONFIG), new PlayScene(SHARED_CONFIG)]
+	scene: [new PreloadScene(), new MenuScene(SHARED_CONFIG), new PlayScene(SHARED_CONFIG)]
 };
 
 //loading assets, such as images, music, animations
---
+++ udemy/game-development-in-js-ts-the-complete-guide/0-flappyBirdClone/src/scenes/PreloadScene.js
@@ -0,0 +1,23 @@
+import Phaser from "phaser";
+
+class PreloadScene extends Phaser.Scene{
+    constructor(){
+        super(' PreloadScence');
+    }
+
+    preload(){
+        this.load.image("sky", "assets/sky.png");
+        this.load.image("bird", "assets/bird.png");
+        this.load.image("pipe", "assets/pipe.png");
+        this.load.image("pauseButton", "assets/pause.png");
+    }
+    create(){
+        this.scene.start('MenuScene');
+    }
+    createBG(){
+        this.add.image(0, 0, "sky").setOrigin(0, 0);
+    }
+
+}
+
+export default PreloadScene
